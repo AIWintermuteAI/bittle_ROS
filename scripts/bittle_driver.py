329e30f83b5f7aed6a6a847bd52551080bffde7c
AIWintermuteAI <dmitrywat@gmail.com>
2020-09-17 23:41:21 +0800

    changed commands

--- scripts/bittle_driver.py
+++ scripts/bittle_driver.py
@@ -29,52 +29,62 @@ class Driver:
         rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
         rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
         
-        cmd = ''
-        cmd = cmd.join([msg.linear.x, msg.linear.y, msg.linear.z,msg.angular.x, msg.angular.y, msg.angular.z])
-        print(cmd)
+        #cmd = ''
+        #cmd = cmd.join([msg.linear.x, msg.linear.y, msg.linear.z,msg.angular.x, msg.angular.y, msg.angular.z])
+        if msg.linear.x > 0:
+            self.wrapper(['kwk',0])
+        elif msg.linear.x < 0:
+            self.wrapper(['kbk',0])
+        elif msg.angular.z > 0:
+            self.wrapper(['kjp',0]) 
+        elif msg.angular.z < 0:
+            self.wrapper(['kbd',0])
+        else:
+            self.wrapper(['kbalance',0])              
+        #print(cmd)
 
         #wrapper([skill_dict[cmd],0])
 
     def run(self):
         rospy.spin()
     
-def wrapper(task):  #Structure is [token, var=[], time]
-    print(task)
-    if len(task)==2:
-        serialWriteByte([task[0]])
-    elif isinstance(task[1][0],int):
-        serialWriteNumToByte(task[0],task[1])
-    else:
-        serialWriteByte(task[1])
-    time.sleep(task[-1])
-    
-def serialWriteNumToByte(token, var=[]): # Only to be used for c m u b i l o within Python
-    #print("Num Token "); print(token);print(" var ");print(var);print("\n\n");
-    if token == 'l' or token=='i':
-        var=list(map(lambda x:int(x), var))
-        instrStr=token+struct.pack('b' * len(var), *var)+'~'
-    elif token =='c' or token =='m' or token =='u' or token =='b':
-        instrStr = token + str(var[0])+" "+str(var[1])+'\n'
-    print("!!!!"+ instrStr)
-    ser.write(instrStr)
+    def wrapper(self, task):  #Structure is [token, var=[], time]
+        print(task)
+        if len(task)==2:
+            self.serialWriteByte([task[0]])
+        elif isinstance(task[1][0],int):
+            self.serialWriteNumToByte(task[0],task[1])
+        else:
+            self.serialWriteByte(task[1])
+        time.sleep(task[-1])
+        
+    def serialWriteNumToByte(self, token, var=[]): # Only to be used for c m u b i l o within Python
+        #print("Num Token "); print(token);print(" var ");print(var);print("\n\n");
+        if token == 'l' or token=='i':
+            var=list(map(lambda x:int(x), var))
+            instrStr=token+struct.pack('b' * len(var), *var)+'~'
+        elif token =='c' or token =='m' or token =='u' or token =='b':
+            instrStr = token + str(var[0])+" "+str(var[1])+'\n'
+        print("!!!!"+ instrStr)
+        self.ser.write(instrStr)
 
-def serialWriteByte(var=[]):
-    token = var[0][0]
-    if (token == 'c' or token == 'm' or token=='b' or token=='u') and len(var)>=2:
-        instrStr=""
-        for element in var:
-            instrStr=instrStr +element+" "
-    elif token == 'l' or token=='i' :
-        if(len(var[0])>1):
-            var.insert(1,var[0][1:])       
-        var[1:]=list(map(lambda x:int(x), var[1:]))
-        instrStr = token+struct.pack('b' * len(var[1:]), *var[1:])+'~'
-    elif token == 'w' or token == 'k':
-        instrStr = var[0] + '\n'
-    else:
-        instrStr = token
-    print("!!!!!!! "+instrStr)
-    ser.write(instrStr)
+    def serialWriteByte(self, var=[]):
+        token = var[0][0]
+        if (token == 'c' or token == 'm' or token=='b' or token=='u') and len(var)>=2:
+            instrStr=""
+            for element in var:
+                instrStr=instrStr +element+" "
+        elif token == 'l' or token=='i' :
+            if(len(var[0])>1):
+                var.insert(1,var[0][1:])       
+            var[1:]=list(map(lambda x:int(x), var[1:]))
+            instrStr = token+struct.pack('b' * len(var[1:]), *var[1:])+'~'
+        elif token == 'w' or token == 'k':
+            instrStr = var[0] + '\n'
+        else:
+            instrStr = token
+        print("!!!!!!! "+instrStr)
+        self.ser.write(instrStr)
     
 if __name__ == '__main__':
     driver = Driver()
